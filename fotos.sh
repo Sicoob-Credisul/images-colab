1b85dd24adcce501f127e23e0200d2b3571710cb
Joao Bazarin <43177303+JoaoBazarin@users.noreply.github.com>
2024-06-28 10:30:25 -0400

    third 100

---
+++ fotos.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+# Nome do arquivo de saída
+output_file="lista_de_arquivos.txt"
+
+# Diretório a ser listado
+directory="."
+
+# Limpa o arquivo de saída, se já existir
+> "$output_file"
+
+# Lista os arquivos no diretório e escreve no arquivo de saída
+ls "$directory" > "$output_file"
+
+echo "Os nomes dos arquivos foram salvos em $output_file"
